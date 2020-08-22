d7bea80c4082ca20798973f06bb0d1e0a84234d7
openupmta <openupmta99@gmail.com>
2020-08-22 14:18:27 +0700

    tien

--- src/Component/Product/ProductDetail.js
+++ src/Component/Product/ProductDetail.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import {
-    useParams
-  } from "react-router-dom";
 class ProductDetail extends Component {
     render() {
-        console.log(this.props.useParams);
         return (
             <div>
-                <h2></h2>
             </div>
         );
     }
